a2ad8643110cf0c85d69664e50406f871b1ee302
osama maher <osama392maher@gmail.com>
2022-10-06 10:44:29 +0200

    Just need some sleep (WOOORKING))

--- main.cpp
+++ main.cpp
@@ -2,48 +2,50 @@
 
 using namespace std;
 
-class BigInt{
-        private:
-            string digits = "";
+class BigDecimalInt{
+private:
+    string digits = "";
+public:
+    // Constructors
+    BigDecimalInt(unsigned long long n);
+    BigDecimalInt(string s);
 
-        public:
+    // Print
+    friend ostream& operator << (ostream& out, BigDecimalInt n);
 
-            // Constructors
-            BigInt(unsigned long long n = 0)
-            {
-                while(n)
-                {
-                    digits.push_back(n % 10);
-                    n /= 10;
-                }
-            }
-    
-            BigInt(string s)
-            {
-                for(int i = 0; i < s.length(); i++)
-                {
-                    if(isdigit(s[i]))
-                    {
-                        digits.push_back(s[i] - '0');
-                    }
-                }
-            }
+};
 
-            // Print
-            friend ostream& operator<<(ostream& out, BigInt n)
-            {
-                for (int i = 0; i < n.digits.size(); i++)
-                {
-                    out << n.digits[i];
-                }
-                out << endl;
-                return out;
-            }
+BigDecimalInt::BigDecimalInt(string s)
+{
+    for(int i = 0; i < s.length(); i++)
+    {
+        digits.push_back(s[i]);
+    }
+}
+BigDecimalInt::BigDecimalInt(unsigned long long n)
+{
+    while(n)
+    {
+        digits.push_back((n % 10) + '0');
+        n /= 10;
+    }
+    reverse(digits.begin(), digits.end());
+}
 
-    };
+ostream& operator << (ostream& out, BigDecimalInt n)
+{
+    for(int i = 0; i < n.digits.length(); i++)
+    {
+        out << n.digits[i];
+    }
+    out << endl;
+    return out;
+}
 
 int main()
 {
-    BigInt n(1234);
+    BigDecimalInt n(1234);
     cout << n;
 }
+
+
